cfb888a4ced773f4e413334085c1f7d9c0f27a2e
Pepe <pepevalbe@users.noreply.github.com>
2017-10-06 09:35:34 +0200

    Update NotesApplication.java

--- src/main/java/com/example/notes/NotesApplication.java
+++ src/main/java/com/example/notes/NotesApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotesApplication{
-
-    @Autowired
-    NotesRepository notesRepository;
     
     public static void main(String[] args) {
         SpringApplication.run(NotesApplication.class, args);
