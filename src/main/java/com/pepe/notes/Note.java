82e3eda3b5c1cdd794d06267ea7577cfe34bf3ef
Pepe <pepevalbe@users.noreply.github.com>
2017-10-10 12:32:12 +0200

    Update Note.java

--- src/main/java/com/pepe/notes/Note.java
+++ src/main/java/com/pepe/notes/Note.java
@@ -8,6 +8,7 @@ package com.pepe.notes;
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Basic;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
