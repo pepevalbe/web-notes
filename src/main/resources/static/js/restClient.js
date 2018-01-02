c1c42c3f8612d60e0f81a3780d86e1eb511dfbb6
pepevalbe <pepevalbe@gmail.com>
2018-01-02 16:47:13 +0100

    fixed js url

--- src/main/resources/static/js/restClient.js
+++ src/main/resources/static/js/restClient.js
@@ -20,18 +20,18 @@ function addText() {
     var payload = new Object();
     payload.text = document.getElementById("text").value;
     var req = new XMLHttpRequest();
-    req.open("POST", "http://localhost:8080/note", false);
+    req.open("POST", "/note", false);
     req.setRequestHeader("Content-type", "application/json");
     req.send(JSON.stringify(payload));
     if (req.status === 201) {
         updateList();
     } else {
-        document.getElementById("output").innerHTML = "Error llamada REST: http://localhost:8080/note";
+        document.getElementById("output").innerHTML = "Error llamada REST: /note";
     }
 }
 
 function findById(id) {
-    var url = "http://localhost:8080/note?id=" + id;
+    var url = "/note?id=" + id;
     var req = new XMLHttpRequest();
     req.open('GET', url, false);
     req.setRequestHeader("Content-type", "application/json");
@@ -46,18 +46,18 @@ function findById(id) {
 
 function findAll() {
     var req = new XMLHttpRequest();
-    req.open('GET', "http://localhost:8080/note/find", false);
+    req.open('GET', "/note/find", false);
     req.setRequestHeader("Content-type", "application/json");
     req.send(null);
     if (req.status === 200) {
         return req.responseText;
     } else {
-        return "Error llamada REST: http://localhost:8080/note/find";
+        return "Error llamada REST: /note/find";
     }
 }
 
 function deleteById(id) {
-    var url = "http://localhost:8080/note?id=" + id;
+    var url = "/note?id=" + id;
     var req = new XMLHttpRequest();
     req.open('DELETE', url, false);
     req.setRequestHeader("Content-type", "application/json");
